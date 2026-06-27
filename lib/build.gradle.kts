25464bda8644816c4c7f4af8dcd37cbc3badd1ef
mrstlib3-dot <mrstlib3@gmail.com>
2026-06-27 20:46:13 +0530

    Remove KotlinCompile options for version settings
    Removed KotlinCompile task configuration for JVM and API versions.

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -16,13 +16,7 @@ repositories {
 }
 
 
-tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
-    kotlinOptions {
-        jvmTarget = "1.8"
-        apiVersion = "1.8"
-        languageVersion = "1.8"
-    }
-}
+
 
 // Force Java compile to use Java 8
 tasks.withType<JavaCompile> {
